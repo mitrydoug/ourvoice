15c8b179b621cf2e0792a67e2addc5de801709d7
Mitchell Douglass <mrdouglass95@gmail.com>
2025-08-17 02:04:14 -0400

    get voting working

--- frontend/src/components/VoteToggle.tsx
+++ frontend/src/components/VoteToggle.tsx
@@ -15,14 +15,14 @@ const VoteToggle: FC<VoteToggleProps> = ({ userVoteCount, onUserVoteChange }) =>
 
   return (
     <Stack spacing="0.5rem" direction="row" >
-        <IconButton aria-label="delete" disabled color="primary" onClick={() => onUserVoteChange(userVoteCount - 1)}>
+        <IconButton color="primary" onClick={() => onUserVoteChange(userVoteCount - 1)}>
             <RemoveCircleOutlineIcon />
         </IconButton>
         <Stack alignItems="center">
             <Typography variant="subtitle1">{userVoteCount}</Typography>
             <Typography variant="body2" sx={{ position: "relative", top: "-10px" }}>Votes</Typography>
         </Stack>
-        <IconButton aria-label="delete" disabled color="primary" onClick={() => onUserVoteChange(userVoteCount + 1)}>
+        <IconButton color="primary" onClick={() => onUserVoteChange(userVoteCount + 1)}>
             <AddCircleOutlineIcon />
         </IconButton>
     </Stack>
