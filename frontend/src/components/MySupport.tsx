5d22753121f413575da4996b5372335b857275ee
Mitchell  Douglass <mrdouglass95@gmail.com>
2025-09-30 21:35:07 -0500

    UI/UX Improvements (#14)

--- frontend/src/components/MySupport.tsx
+++ frontend/src/components/MySupport.tsx
@@ -1,9 +1,8 @@
-import { Card, CardContent, Stack, Typography } from "@mui/material";
 import React, { FC } from "react";
 import { useReadContract } from "wagmi";
 import { forumContractConfig } from "../contracts";
 import { useUserVotes } from "../state/UserVotes";
-import VoteToggle from "./VoteToggle";
+import StatementCard from "./StatementCard";
 
 interface Statement {
   id: bigint;
@@ -16,7 +15,6 @@ interface Statement {
 const MySupport: FC = () => {
   const {
     state: { userVotes },
-    dispatch,
   } = useUserVotes();
 
   console.log("User votes map: ", userVotes);
@@ -42,32 +40,7 @@ const MySupport: FC = () => {
   return (
     <>
       {myStatements?.map((stmt, idx) => (
-        <Card key={`stmt-${idx}`}>
-          <CardContent>
-            <Stack
-              direction="row"
-              spacing={2}
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              <Typography variant="h5" component="div">
-                {stmt.rank + 1n} - {stmt.text}
-              </Typography>
-              <Typography variant="h5" component="div">
-                {stmt.voteCount}
-              </Typography>
-              <VoteToggle
-                userVoteCount={userVotes?.get(Number(stmt.id)) || 0}
-                onUserVoteChange={(n) =>
-                  dispatch({
-                    type: "UPDATE_VOTE",
-                    payload: { statementId: stmt.id, newVoteCount: BigInt(n) },
-                  })
-                }
-              />
-            </Stack>
-          </CardContent>
-        </Card>
+        <StatementCard key={`stmt-${idx}`} statement={stmt} />
       ))}
     </>
   );
--- frontend/src/components/VoteToggle.tsx
+++ frontend/src/components/VoteToggle.tsx
@@ -1,40 +1,51 @@
 import React, { FC } from "react";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import Stack from "@mui/material/Stack";
+import { Box } from "@mui/material";
 
 interface VoteToggleProps {
   userVoteCount: number;
+  uncommitedVote: boolean;
   onUserVoteChange: (newVoteCount: number) => void;
 }
 
 const VoteToggle: FC<VoteToggleProps> = ({
   userVoteCount,
+  uncommitedVote,
   onUserVoteChange,
 }) => {
   return (
-    <Stack spacing="0.5rem" direction="row">
-      <IconButton
-        color="primary"
-        onClick={() => onUserVoteChange(userVoteCount - 1)}
-      >
-        <RemoveCircleOutlineIcon />
-      </IconButton>
-      <Stack alignItems="center">
-        <Typography variant="subtitle1">{userVoteCount}</Typography>
-        <Typography variant="body2" sx={{ position: "relative", top: "-10px" }}>
-          Votes
+    <Box
+      sx={{
+        border: "1px solid lightgray",
+        borderRadius: "50vh",
+        p: 0,
+        backgroundColor: uncommitedVote ? "lightyellow" : "transparent",
+      }}
+    >
+      <Stack spacing="0.1rem" direction="row" alignItems="center">
+        <IconButton
+          color="primary"
+          onClick={() => onUserVoteChange(userVoteCount - 1)}
+          sx={{ p: 0.2 }}
+        >
+          <RemoveIcon />
+        </IconButton>
+        <Typography color="text.secondary" sx={{ position: "relative" }}>
+          {userVoteCount}
         </Typography>
+        <IconButton
+          color="primary"
+          onClick={() => onUserVoteChange(userVoteCount + 1)}
+          sx={{ p: 0.2 }}
+        >
+          <AddIcon />
+        </IconButton>
       </Stack>
-      <IconButton
-        color="primary"
-        onClick={() => onUserVoteChange(userVoteCount + 1)}
-      >
-        <AddCircleOutlineIcon />
-      </IconButton>
-    </Stack>
+    </Box>
   );
 };
 
