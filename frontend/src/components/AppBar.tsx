0e886c13b0f303e5670e78519045a9836e3d35df
Mitchell Douglass <mrdouglass95@gmail.com>
2026-07-07 21:22:43 -0400

    format

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -157,7 +157,7 @@ export default function MenuAppBar() {
                       disconnect={doDisconnect}
                       username={displayName}
                       avatar={avatar}
-                      commitChanges={commitChanges ?? (() => { })}
+                      commitChanges={commitChanges ?? (() => {})}
                       hasStagedChanges={
                         userVoteState?.hasStagedChanges ?? false
                       }
