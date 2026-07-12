c39f800c28802a228fc54b34735e04681b1536f7
Mitchell Douglass <mrdouglass95@gmail.com>
2026-07-12 15:56:09 -0400

    update dark theme logo

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -12,6 +12,7 @@ import ForumIcon from "./ForumIcon";
 import { useForum, forumToSlug } from "../state/Forum";
 import useIsMobile from "@/hooks/useIsMobile";
 import useUserIdentity from "@/hooks/useUserIdentity";
+import useLogoSrc from "@/hooks/useLogoSrc";
 import { theme } from "../theme";
 import { ProfileDrawer } from "./UserProfileMenu";
 import { useSearchQuery } from "@/state/Search";
@@ -45,10 +46,11 @@ interface LogoProps {
 }
 
 const Logo: React.FC<LogoProps> = ({ isMobile, homePath }) => {
+  const logoSrc = useLogoSrc();
   return (
     <Link to={homePath} style={{ textDecoration: "none" }}>
       <img
-        src="./symvolia-logo.svg"
+        src={logoSrc}
         alt="Symvolia"
         style={{
           height: isMobile
@@ -157,7 +159,7 @@ export default function MenuAppBar() {
                       disconnect={doDisconnect}
                       username={displayName}
                       avatar={avatar}
-                      commitChanges={commitChanges ?? (() => {})}
+                      commitChanges={commitChanges ?? (() => { })}
                       hasStagedChanges={
                         userVoteState?.hasStagedChanges ?? false
                       }
