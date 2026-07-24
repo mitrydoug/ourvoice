135be410bd43e89a031415e059b802560c5adb03
Mitchell Douglass <mrdouglass95@gmail.com>
2026-07-23 22:24:31 -0400

    format

--- frontend/src/components/StatementCard.tsx
+++ frontend/src/components/StatementCard.tsx
@@ -199,7 +199,7 @@ export const StatementCard: FC<StatementCardProps> = ({
   const { forumContractAddress } = useForum();
   const { toCredits, toParts } = useCreditConversion();
 
-  const { blockNumber } = useBlockSync(() => { });
+  const { blockNumber } = useBlockSync(() => {});
 
   const { data: historicalData } = useReadContract({
     address: forumContractAddress,
@@ -253,13 +253,13 @@ export const StatementCard: FC<StatementCardProps> = ({
     const adjustment =
       newCreditSupport === 0
         ? {
-          value: 0,
-          adjustmentType: SupportAdjustmentType.SetTo,
-        }
+            value: 0,
+            adjustmentType: SupportAdjustmentType.SetTo,
+          }
         : {
-          value: toParts(newCreditSupport - onChainCredits),
-          adjustmentType: SupportAdjustmentType.Delta,
-        };
+            value: toParts(newCreditSupport - onChainCredits),
+            adjustmentType: SupportAdjustmentType.Delta,
+          };
     dispatch({
       type: "STAGE_USER_SUPPORT",
       payload: {
@@ -275,9 +275,9 @@ export const StatementCard: FC<StatementCardProps> = ({
   const rankingProgress =
     rankingThreshold !== undefined
       ? rankingProgressPercent(
-        Number(statement.support),
-        Number(rankingThreshold),
-      )
+          Number(statement.support),
+          Number(rankingThreshold),
+        )
       : null;
 
   const globalSupport = toCredits(Number(statement.support));
